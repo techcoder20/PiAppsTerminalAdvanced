4aac98553b78b319c729594c01f698816f722112
RPICoder <68472305+techcoder20@users.noreply.github.com>
2021-02-13 13:17:23 +0000

    Update install.sh

--- install.sh
+++ install.sh
@@ -2,8 +2,7 @@
 
 wget https://raw.githubusercontent.com/techcoder20/PiAppsPackageManagerTerminal/main/papm.py
 mv papm.py ~/pi-apps
-alias papm='python3 ~/pi-apps/papm.py'
 echo "alias papm='python3 ~/pi-apps/papm.py'" >> ~/.bashrc
 alias papm='python3 ~/pi-apps/papm.py'
+bash
 papm help
-alias papm='python3 ~/pi-apps/papm.py'
