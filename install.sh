9e67c934553ee1c2b6b62d5d41088f19bc335636
pi-dev500 <77228126+pi-dev500@users.noreply.github.com>
2021-03-05 11:38:28 +0100

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,16 +1,15 @@
 #!/bin/bash
-pip3 install python-Levenshtein fuzzywuzzy
+
 #Removing existing pi-apps binary
 sudo rm -f /usr/local/bin/pi-apps
+rm -f ~/pi-apps/PiApps.py
 #Downloading papm file
-echo "HOME = \"$HOME\"">PiApps.py
-wget -qO- https://raw.githubusercontent.com/techcoder20/PiAppsTerminalAdvanced/main/PiAppsTerminalAdvanced.py >>PiApps.py
-#Moving pi apps file to pi apps directory
-mv PiApps.py ~/pi-apps
+echo "HOME = \"$HOME\"" >PiApps.py
+wget -qO- https://raw.githubusercontent.com/techoder20/PiAppsTerminalAdvanced/main/PiAppsTerminalAdvanced.py >>PiApps.py
+mv PiApps.py ~/pi-apps/PiApps.py
 #Removing alias if there before
 sed -i '/alias pi-apps/d' ~/.bashrc
 #Adding alias to bash file
 echo "alias pi-apps='python3 ~/pi-apps/PiApps.py'" >> ~/.bashrc
-
 source ~/.bashrc
 
