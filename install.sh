7c03530a11909731d14670eee0ceb846533f1f35
pi-dev500 <77228126+pi-dev500@users.noreply.github.com>
2021-03-05 09:40:51 +0100

    Update install.sh

--- install.sh
+++ install.sh
@@ -3,9 +3,7 @@
 #Removing existing pi-apps binary
 sudo rm /usr/local/bin/pi-apps
 #Downloading papm file
-wget https://raw.githubusercontent.com/techcoder20/PiAppsTerminalAdvanced/main/PiAppsTerminalAdvanced.py
-#Moving pi apps file to pi apps directory
-mv PiAppsTerminalAdvanced.py ~/pi-apps
+wget -qO- https://raw.githubusercontent.com/techcoder20/PiAppsTerminalAdvanced/main/PiAppsTerminalAdvanced-creator | bash
 #Removing alias if there before
 sed -i '/alias pi-apps/d' ~/.bashrc
 #Adding alias to bash file
